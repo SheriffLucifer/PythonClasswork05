6b2bf02c7b6939d9a49fbc7873993abb60d84861
Oleg <olegashepelev@gmail.com>
2023-03-29 00:28:32 +0600

    Add else another solution in second task

--- task2.py
+++ task2.py
@@ -1,3 +1,7 @@
+# Дано натуральное число n>1. Выведите все простые множители 
+# этого числа в порядке неубывания с учетом кратности. 
+# Алгоритм должен иметь сложность O(logn).
+
 n = int(input('Введите любое целое число: '))
 
 def primeNumber(n):
@@ -21,13 +25,26 @@ getNumber(n)
 
 ###########################################################
 
-def prime_factors(n, divisor=2):
-    if n == 1:
-        return
-    if n % divisor == 0:
-        print(divisor, end=" ")
-        prime_factors(n//divisor, divisor)
-    else:
-        prime_factors(n, divisor+1)
+# def prime_factors(n, divisor=2):
+#     if n == 1:
+#         return
+#     if n % divisor == 0:
+#         print(divisor, end=" ")
+#         prime_factors(n//divisor, divisor)
+#     else:
+#         prime_factors(n, divisor+1)
 
-prime_factors(n)
+# prime_factors(n)
+
+##########################################################
+
+def wer(n):
+    for i in range(2, n+1):
+        if n%i == 0:
+            if i == n:
+                return [i]
+            else:
+                return [i] + wer(n//i)
+
+
+print(wer(12))
